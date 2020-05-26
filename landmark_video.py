9b5ee2aae682c7f4785d098bcc6837f07def31ac
kimh060612 <kimh060612@naver.com>
2020-05-27 01:58:39 +0900

    .

--- landmark_video.py
+++ landmark_video.py
@@ -5,6 +5,7 @@ from skimage import io
 import collections
 import cv2
 
+
 pred_type = collections.namedtuple('prediction_type', ['slice', 'color'])
 pred_types = {'face': pred_type(slice(0, 17), (0.682, 0.780, 0.909, 0.5)),
               'eyebrow1': pred_type(slice(17, 22), (1.0, 0.498, 0.055, 0.4)),
@@ -28,8 +29,8 @@ if __name__ == "__main__":
 
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cuda', flip_input=True)
 
-    VideoPath = "./test/MyHeroTest.avi"
-    OutVideo = "./test/MyHeroResult.avi"
+    VideoPath = "./test/NigeHazi.avi"
+    OutVideo = "./test/NigeHaziResult.avi"
     Vid = cv2.VideoCapture(VideoPath)
 
     frame_width = Vid.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -37,23 +38,32 @@ if __name__ == "__main__":
     print(frame_width, frame_height)
 
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-    out = cv2.VideoWriter(OutVideo, fourcc, 25.0, (640,360))
+    out = cv2.VideoWriter(OutVideo, fourcc, 25.0, (1280,720))
 
     while Vid.isOpened():
-        ret, frame = Vid.read()
         try:
+            ret, frame = Vid.read()
             Pri_index = 0
-            preds = fa.get_landmarks(io.imread(frame))[-1]
-            for pred_type in pred_types.values():
-                Det = len(check[pred_type.slice])
-                for i in range(Det):
-                    frame = cv2.line(frame, (preds[Pri_index + i, 0],preds[Pri_index + i, 1]), (preds[Pri_index + i, 0],preds[Pri_index + i, 1]) , (0, 0, 255), 5) 
-                Pri_index += (Det - 1)
-        except :
-            print("No Face detected")
+            #input_img = io.imread(frame)
+            K = fa.get_landmarks(frame)
+            if not K == None :
+                preds = K[-1]
+                if not len(preds) == 0:
+                    for pred_type in pred_types.values():
+                        Det = len(check[pred_type.slice])
+                        for i in range(Det):
+                            frame = cv2.line(frame, (preds[Pri_index + i, 0],preds[Pri_index + i, 1]), (preds[Pri_index + i, 0],preds[Pri_index + i, 1]) , (0, 0, 255), 5) 
+                        Pri_index += (Det - 1)
+                else :
+                    print("Failed")
 
-        out.write(frame)
-        cv2.imshow('video', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            out.write(frame)
+            cv2.imshow('video', frame)
+        except :
+            print("Failed")
 
 
 
